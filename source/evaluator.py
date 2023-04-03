35b47166e13299ecaa2a27928e34e2e9ea5ef50f
Alexey Rebrikov <rebrikov.av@phystech.edu>
2023-04-03 11:26:09 +0300

    Add Problem master class

--- source/evaluator.py
+++ source/evaluator.py
@@ -1,3 +1,19 @@
+import numpy as np
 
-def greet():
-    print('hi')
+class Problem:
+    def __init__(self, ref_ans:str, max_pts: int, min_share: float = 0.5) -> None:
+        self.ref_ans = ref_ans
+        self.min_share = min_share
+        self.max_pts = max_pts
+
+
+    def evaluate(self, evl_ans: str) -> int:
+        share = self.evaluate_share(evl_ans)
+        pts = (share - self.min_share) / (1 - self.min_share) * self.max_pts
+        return int(max(pts, 0))
+        
+
+    def evaluate_share(self, evl_ans : str) -> float:
+        raise NotImplementedError('can\'t evaluate share for this problem type')
+    
+
