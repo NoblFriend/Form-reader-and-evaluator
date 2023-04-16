559bf9904ccf88ba660dc917f90f60da404dfa70
Alexey Rebrikov <rebrikov.av@phystech.edu>
2023-04-16 18:39:02 +0300

    Add a wrapper for recognition of the blank

--- source/blank_reader.py
+++ source/blank_reader.py
@@ -1,47 +1,49 @@
 import json
 import cv2
 import numpy as np
+import pandas as pd
 from config import config
 from tensorflow.keras.models import load_model
 
 
 class BlankReader:
-    def __init__(self) -> None:
-        self._model = load_model('model/AM_50.model')
-        with open('dump/data.json', 'r') as f:
+    def __init__(self, info_file='dump/data.json', model='model/AM_50.model') -> None:
+        self._model = load_model(model)
+        with open(info_file, 'r') as f:
             self._ref_coords = json.load(f)
-        self._img_coords = {'QR': dict()}
-        self._letters = dict()
+        self.recognized_data = dict()
+        self.recognized_data['Fields'] = self._ref_coords['Fields']
+        self.table_code_answers = pd.DataFrame(columns=[f'q{i+1}' for i in range(len(self._ref_coords['Fields']))])
+        self.table_code_answers.index.name = 'code'
 
-    def set_blank(self, name):
-        self._name = name
-        self._canvas = cv2.imread(f'blanks/{name}.png')
+    def _set_blank(self, path):
+        self._cur_coords = {'QR': dict()}
+        self._cur_letters = dict()
+        self._cur_canvas = cv2.imread(f'{path}.png')
 
-    def save_blank(self, name=None):
-        if name == None:
-            name = self._name
-        cv2.imwrite(f'blanks/processed_{name}.png', self._canvas)
+    def _save_blank(self, path):
+        cv2.imwrite(f'{path}_processed.png', self._cur_canvas)
 
-    def recover_blank(self):
-        _, data, points, _ = cv2.QRCodeDetector().detectAndDecodeMulti(self._canvas)
+    def _recover_blank(self):
+        _, data, points, _ = cv2.QRCodeDetector().detectAndDecodeMulti(self._cur_canvas)
         unrecognized = False
         for d, p in zip(data, points):
             try:
-                self._img_coords['QR'][d.split('|')[0]] = [p[0], p[2]]
-                self._code = d.split('|')[1]
+                self._cur_coords['QR'][d.split('|')[0]] = [p[0], p[2]]
+                self._cur_code = d.split('|')[1]
             except:
                 if (not unrecognized):
                     unrecognized = True
                 else:
                     raise RuntimeError(
-                        f'Too many (at least 2) unrecognized qr codes on {self._name}')
+                        f'Too many (at least 2) unrecognized qr codes on {self._path}')
 
         # if only 2 recognized will be implemented
 
-        self._canvas = cv2.warpAffine(src=self._canvas,
-                                      M=cv2.getAffineTransform(src=np.float32([self._img_coords['QR']['bot_left'][1],
-                                                                               self._img_coords['QR']['top_left'][1],
-                                                                               self._img_coords['QR']['top_right'][1]]),
+        self._cur_canvas = cv2.warpAffine(src=self._cur_canvas,
+                                      M=cv2.getAffineTransform(src=np.float32([self._cur_coords['QR']['bot_left'][1],
+                                                                               self._cur_coords['QR']['top_left'][1],
+                                                                               self._cur_coords['QR']['top_right'][1]]),
                                                                dst=np.float32([self._ref_coords['QR']['bot_left'][1],
                                                                                self._ref_coords['QR']['top_left'][1],
                                                                                self._ref_coords['QR']['top_right'][1]])),
@@ -49,16 +51,16 @@ class BlankReader:
                                              config.page.height),
                                       flags=cv2.INTER_LINEAR)
 
-    def find_boxes(self):
+    def _find_boxes(self):
         for row_key, row in self._ref_coords['Fields'].items():
-            self._letters[row_key] = dict()
+            self._cur_letters[row_key] = dict()
             for box_key, box in row.items():
                 delta = 3 * self._ref_coords['thickness']
-                x1 = box[0][0]
-                y1 = box[0][1]
-                x2 = box[1][0]
-                y2 = box[1][1]
-                roi = self._canvas[y1 - delta:y2+delta, x1 - delta:x2 + delta]
+                x1 = box[0][0] - delta
+                y1 = box[0][1] - delta
+                x2 = box[1][0] + delta
+                y2 = box[1][1] + delta
+                roi = self._cur_canvas[y1:y2, x1:x2]
                 gray_img = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
                 # # Применение пороговой обработки для создания бинарного изображения
@@ -110,25 +112,35 @@ class BlankReader:
                 resized_img = cv2.resize(square_img, (32, 32), interpolation=cv2.INTER_AREA)
                 # cv2.imshow('a', resized_img)
                 # cv2.waitKey()
-                self._letters[row_key][box_key] = np.expand_dims(resized_img.astype("float32") / 255.0, axis=-1)
+                self._cur_letters[row_key][box_key] = np.expand_dims(resized_img.astype("float32") / 255.0, axis=-1)
 
-    def recognize_letters(self):
-        self._predictions = dict()
+    def _recognize_letters(self):
+        self._cur_predictions = dict()
         label_names="ABCDEFGHIJKLM_"
-        for row_key, row in self._letters.items():
-            self._predictions[row_key] = dict()
+        for row_key, row in self._cur_letters.items():
+            self._cur_predictions[row_key] = dict()
             for box_key, box in row.items():
                 array = np.array([box], dtype="float32")
                 pred = self._model.predict(array)[0]
-                self._predictions[row_key][box_key] = dict({'pred' :  round(float(np.max(pred)),2), 'ans' :  label_names[np.argmax(pred)]})
+                self._cur_predictions[row_key][box_key] = dict({'pred' :  round(float(np.max(pred)),2), 'ans' :  label_names[np.argmax(pred)]})
         
         with open('dump/pred.json', 'w') as f:
-            json.dump(self._predictions, f, indent=4)
+            json.dump(self._cur_predictions, f, indent=4)
+    
+    def recognize_answers(self, path):
+        self._set_blank(path)
+        self._recover_blank()
+        self._find_boxes()
+        self._recognize_letters()
+
+        self.recognized_data[self._cur_code] = self._cur_predictions
+        self.table_code_answers.loc[self._cur_code]= [''.join([box['ans'] for box in row.values()]) for row in self._cur_predictions.values()]
+    
+# def 
 
 if __name__ == '__main__':
     reader = BlankReader()
-    reader.set_blank('test')
-    reader.recover_blank()
-    reader.find_boxes()
-    reader.recognize_letters()
-    reader.save_blank()
+    reader.recognize_answers('sandbox/blanks/test')
+
+    print(reader.recognized_data)
+
