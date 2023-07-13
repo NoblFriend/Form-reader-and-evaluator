1eac12f1efe25ebc4e3a21701e5021a5836b64e4
Alexey Rebrikov <rebrikov.av@phystech.edu>
2023-07-13 10:40:11 +0300

    Change page params

--- app/source/config.py
+++ app/source/config.py
@@ -13,20 +13,23 @@ class Configuration:
             self.row_spacing = 60
             self.box_size = 70
             self.box_spacing = 20
-            self.x_step = self.box_size + self.box_spacing
             self.y_step = self.box_size + self.row_spacing
-
+            self.number_spacing = 10
+            self.number_height = 20
+            self.thickness = 4
 
     class QR:
         def __init__(self, page):
             self.box_size = 9
             self.size = 21 * self.box_size
-            self.tl = (page.margin, page.margin,
-                             page.margin + self.size, page.margin + self.size)
-            self.tr = (page.width - self.size - page.margin, page.margin,
-                              page.width - page.margin, page.margin + self.size)
-            self.bl = (page.margin, page.height - self.size - page.margin,
-                             page.margin + self.size, page.height - page.margin)
+            self.coords = {
+                'tl': [[page.margin, page.margin],
+                       [page.margin + self.size, page.margin + self.size]],
+                'tr': [[page.width - self.size - page.margin, page.margin],
+                       [page.width - page.margin, page.margin + self.size]],
+                'bl': [[page.margin, page.height - self.size - page.margin],
+                       [page.margin + self.size, page.height - page.margin]]
+            }
 
     def __init__(self):
         self.page = self.Page()
